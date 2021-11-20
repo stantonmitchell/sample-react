680ff405440f3542c259e1c813276611d264dd03
Mitchell Stanton <mitchell.stanton@cyber76.com>
2021-11-20 18:32:50 -0500

    Update for cleanup and maintenance

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sample React on Docker
+
+Simply a sample React app deployment.
+
+## How to use
+
+1. Clone the repo: `git clone git@github.com:stantonmitchell/sample-react.git`
+1. Change to the repository directory: `cd sample-react`
+1. Deploy the app: `docker-compose up -d`
+1. Wait for the image download and deployment to finish.
+1. Navigate in your browser to: `http://localhost`
+
+### Cleanup
+
+1. Run `chmod +x ./cleanup.sh`
+1. Run `./cleanup.sh`
---
+++ cleanup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo "========================================"
+echo "       RUNNING CONTAINER CLEANUP"
+CONTAINERS=`docker ps -a -f NAME=myReactapp --format '{{.Names}}'`
+CONTAINER_CHECK=`docker ps -a -f NAME=myReactapp --format '{{.Names}}' | wc -l`
+if [[ "$CONTAINER_CHECK" -gt "0" ]]; then
+   echo "Containers online..removing"
+   docker rm -f $CONTAINERS
+else
+   echo "Found no containers..skipping removal"
+fi
+echo "========================================"
+echo "         RUNNING IMAGE CLEANUP"
+# Image Version Config
+NGINX_BASE_IMAGE='nginx:1.21.3'
+NGINX_CUSTOM_IMAGE='react-sample_nginx:latest'
+NODE_BASE_IMAGE='node:12.19.1'
+NODE_CUSTOM_IMAGE='react-sample:latest'
+# Start NGINX Image 
+echo "---> Removing NGINX images"
+NGINX_BASE_CHECK=`docker images -f reference="$NGINX_BASE_IMAGE" --format '{{.Repository}}:{{.Tag}}' | wc -l`
+NGINX_CUSTOM_CHECK=`docker images -f reference="$NGINX_CUSTOM_IMAGE" --format '{{.Repository}}:{{.Tag}}' | wc -l`
+if [[ "$NGINX_BASE_CHECK" == "1" ]]; then
+   echo "$NGINX_BASE_IMAGE image exist..removing"
+   docker image remove "$NGINX_BASE_IMAGE"
+else
+   echo "$NGINX_BASE_IMAGE not found..skipping removal"
+fi
+if [[ "$NGINX_CUSTOM_CHECK" == "1" ]]; then
+   echo "$NGINX_CUSTOM_IMAGE image exist..removing"
+   docker image remove "$NGINX_CUSTOM_IMAGE"
+else
+   echo "$NGINX_CUSTOM_IMAGE not found..skipping removal"
+fi
+echo "---> Removing NODE images"
+# Start NODE Image
+NODE_BASE_CHECK=`docker images -f reference="$NODE_BASE_IMAGE" --format '{{.Repository}}:{{.Tag}}' | wc -l`
+NODE_CUSTOM_CHECK=`docker images -f reference="$NODE_CUSTOM_IMAGE" --format '{{.Repository}}:{{.Tag}}' | wc -l`
+if [[ "$NODE_BASE_CHECK" == "1" ]]; then
+   echo "$NODE_BASE_IMAGE image exist..removing"
+   docker image remove "$NODE_BASE_IMAGE"
+else
+   echo "$NODE_BASE_IMAGE not found..skipping removal"
+fi
+if [[ "$NODE_CUSTOM_CHECK" == "1" ]]; then
+   echo "$NODE_CUSTOM_IMAGE image exist..removing"
+   docker image remove "$NODE_CUSTOM_IMAGE"
+else
+   echo "$NODE_CUSTOM_IMAGE not found..skipping removal"
+fi
+echo "---> All images removed"
+echo "========================================"
+echo "Script completed"
